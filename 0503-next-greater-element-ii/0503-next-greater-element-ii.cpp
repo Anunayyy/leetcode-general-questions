9013e9e4990251845aed24460934bc1d29abe7c0
Anunay <94173658+Anunayyy@users.noreply.github.com>
2023-03-30 17:11:12 +0530

    Time: 30 ms (92.19%), Space: 24.4 MB (33.00%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        //vector<int>ans;
+        stack<int>s;
+        int n = nums.size();
+        vector<int>ans(n,0);
+        
+        
+        for(int i=n-1;i>=0;i--){
+            s.push(nums[i]);
+        }
+        
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty() and nums[i]>=s.top()) s.pop();
+            if(s.empty())ans[i]=-1;
+            else ans[i]=s.top();
+            s.push(nums[i]);
+        }
+        return ans;
+        
+    }
+};
+/*
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans(n,0);
+        stack<int>s;
+        for(int i=n-1;i>=0;i--) {
+            s.push(nums[i]);
+        }
+        
+        for(int i=n-1;i>=0;i--) {
+            while(!s.empty() && nums[i]>=s.top()) s.pop();
+            if(s.empty()) ans[i]=-1;
+            else ans[i]=s.top();
+            s.push(nums[i]);
+        }
+        return ans;
+    }
+};
+*/
