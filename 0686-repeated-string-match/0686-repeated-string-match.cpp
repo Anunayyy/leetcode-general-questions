a0e1c6f25c751c06fe163bcabb32700c6206c722
Anunay <94173658+Anunayyy@users.noreply.github.com>
2023-04-01 13:05:04 +0530

    Time: 8 ms (79.06%), Space: 7.1 MB (38.37%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -3,6 +3,7 @@ public:
     int repeatedStringMatch(string a, string b) {
         string s="";
         int cnt=0;
+        //if(a.find(b[0]==string::npos)) return -1;
         
         while(s.size()<b.size()){
             s+=a;
