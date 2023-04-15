d04c644f9b1321717bd334831d13013f7922adfb
Anunay <94173658+Anunayyy@users.noreply.github.com>
2023-04-15 21:58:16 +0530

    Update 0073-set-matrix-zeroes.cpp

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
+        int m = matrix.size();  // row size
+        int n = matrix[0].size(); // col size
         
         unordered_set<int>set_row;
         unordered_set<int>set_col;
@@ -18,7 +18,7 @@ public:
         
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(set_row.count(i)>0 or set_col.count(j)>0) matrix[i][j]=0;
+                if(set_row.count(i)>0 || set_col.count(j)>0) matrix[i][j]=0;
             }
         }
         
