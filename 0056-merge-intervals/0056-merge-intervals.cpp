632320ed0d712651da0a2db1faa8f8343910b390
Anunay <94173658+Anunayyy@users.noreply.github.com>
2023-04-17 01:55:39 +0530

    Time: 43 ms (50.55%), Space: 19.1 MB (75.92%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+         
+        if(intervals.size()<=1) return intervals;
+         
+        sort(intervals.begin(), intervals.end());
+         
+        vector<vector<int>> output;
+        output.push_back(intervals[0]);
+         
+        for(int i=1; i<intervals.size(); i++) {
+            if(output.back()[1] >= intervals[i][0]) output.back()[1] = max(output.back()[1] , intervals[i][1]);
+            else output.push_back(intervals[i]); 
+        }
+        return output;
+    }
+};
+
+
+/*
+here we made a vector to store all the answers. now we push the first array then we compare if the last element of the array in the vector is larger than the first element of the next array. if yes it merges. so we put the max or the two elecments and merge the array
+*/
+
