40290223ace49d29985ef7aca52dc99130b0948c
Anunay <94173658+Anunayyy@users.noreply.github.com>
2023-04-17 01:22:16 +0530

    Time: 7 ms (46.22%), Space: 11.3 MB (40.71%) - LeetHub

---
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+		// we first make the transpose of the matrix
+        int n = matrix.size();
+        for(int i=0; i<n; ++i) {
+            for(int j=i; j<n; ++j) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        for(int i=0; i<n; ++i) {
+		// we then using the 2 pointer approach swap the columns. it is really easy to imagine of you use a 3x3 matrix in your mind. even a 4x4 is easy.
+            int left = 0, right = n-1;
+            while(left < right) {
+                swap(matrix[i][left], matrix[i][right]);
+                ++left;
+                --right;
+            }
+        }
+    }
+        
+        
+    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        
+        if(mat==target) return true;
+        int i=0;
+        while(i<3){
+            rotate(mat);
+            if(mat==target){
+                return true;
+                break;
+            }
+            else ++i;
+            
+        }
+        return false;
+        
+        
+    }
+};
