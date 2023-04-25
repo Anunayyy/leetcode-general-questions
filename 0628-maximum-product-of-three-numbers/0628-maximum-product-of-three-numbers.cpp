f9535c8a7b290df980e19650ad7e4f41299a9b9b
Anunay <94173658+Anunayyy@users.noreply.github.com>
2023-04-25 19:28:42 +0530

    Time: 59 ms (16.08%), Space: 27.6 MB (99.30%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -10,7 +10,7 @@ public:
         // 1. the product of the last 3 elemts would yeild the desired result
         //2. if the first 2 elemetns are negative and the multiplication can cancel each other with the 3rd element...i.e possibly positive or
         // 3. the first 2 elements which are negative and the the last element with is possibly positive could yeild the result.
-        return max(a[n-1]*a[n-2]*a[n-3],max(a[0]*a[1]*a[2],a[0]*a[1]*a[n-1]));
+        return max(a[n-1]*a[n-2]*a[n-3],a[0]*a[1]*a[n-1]);
         
     }
 };
