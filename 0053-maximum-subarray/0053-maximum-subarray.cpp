7bb2001e721b58a527414caef6f8e5e8549438b9
Anunay <94173658+Anunayyy@users.noreply.github.com>
2023-06-09 10:53:33 +0530

    Time: 119 ms (69.00%), Space: 68.2 MB (7.91%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int curr = 0;
-        int max_sum = INT_MIN;
-        for(int i=0;i<nums.size();++i){
-            if(curr<0) curr=0;
-             curr += nums[i];
-            
-            max_sum = max(max_sum, curr); 
-            
+        int n= nums.size();
+        int dp[n];
+        dp[0]=nums[0];
+        
+        int maxsum = nums[0];
+        for(int i=1;i<n;i++){
+            dp[i]=nums[i] + (dp[i-1]>0 ? dp[i-1]:0);
+            maxsum = max(maxsum,dp[i]);
         }
-        return max_sum;
+        return maxsum;
         
     }
 };
-
-/* 
-we kept a variable curr. it'll traverse througg the whole array and add it up, if it's positive. then we copare it with mx and store the mx.
-*/
