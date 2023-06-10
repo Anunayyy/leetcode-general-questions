b0a46e0b8b858ae23843239b23ed2c778d327cd9
Anunay <94173658+Anunayyy@users.noreply.github.com>
2023-06-10 17:02:05 +0530

    Time: 49 ms (27.80%), Space: 19 MB (54.84%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-         
-        if(intervals.size()<=1) return intervals;
-         
-        sort(intervals.begin(), intervals.end());
-         
-        vector<vector<int>> output;
-        output.push_back(intervals[0]);
-         
-        for(int i=1; i<intervals.size(); i++) {
-            if(output.back()[1] >= intervals[i][0]) output.back()[1] = max(output.back()[1] , intervals[i][1]);
-            else output.push_back(intervals[i]); 
+    vector<vector<int>> merge(vector<vector<int>>& interval) {
+        vector<vector<int>>result;
+        int n = interval.size();
+        
+        if (n<=1) return interval;
+        
+        sort(interval.begin(),interval.end());
+        
+        result.push_back(interval[0]);
+        
+        for(int i=1;i<n;i++){
+            if(result.back()[1]>=interval[i][0]) result.back()[1] = max(result.back()[1],interval[i][1]);
+            else result.push_back(interval[i]);
         }
-        return output;
+        return result;
+        
+        
     }
 };
-
-
-/*
-here we made a vector to store all the answers. now we push the first array then we compare if the last element of the array in the vector is larger than the first element of the next array. if yes it merges. so we put the max or the two elecments and merge the array
-*/
-
