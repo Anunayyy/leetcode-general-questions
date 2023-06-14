0d95baa0b2c6fe497657b0e7a02aa8175e275c4b
Anunay <94173658+Anunayyy@users.noreply.github.com>
2023-06-14 14:05:51 +0530

    Time: 40 ms (58.04%), Space: 14.6 MB (18.02%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         priority_queue<int>pq;
@@ -13,6 +13,23 @@ public:
         
     }
 };
-
+*/
 /*
 here we add the elements to a max heap. it the size of the heap gets more than k then it pops the elments. at the end only k elements are present in the heap*/
+
+//using a vector
+class Solution {
+public:
+        int kthSmallest(vector<vector<int>>& matrix, int k) {
+        vector<int> ans;
+        int n=matrix.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                ans.push_back(matrix[i][j]);
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans[k-1];
+    }
+};
+
