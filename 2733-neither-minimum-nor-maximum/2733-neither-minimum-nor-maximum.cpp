9df1ceacd8ad20ee01a6953829b5fbb7e731b411
Anunay <94173658+Anunayyy@users.noreply.github.com>
2023-06-14 23:35:25 +0530

    Time: 178 ms (7.08%), Space: 94.8 MB (6.53%) - LeetHub

--- 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.cpp
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.cpp
@@ -4,10 +4,21 @@ public:
         int n = nums.size();
         
         if(n<=2) return -1;
+        //vector
+        /*sort(nums.begin(),nums.end());
         
-        sort(nums.begin(),nums.end());
+        return nums[n-2];*/
         
-        return nums[n-2];
+        priority_queue<int>pq;
+        
+        for(auto x:nums){
+            pq.push(x);
+            }
+        pq.pop();
+        
+        return pq.top();
+        
+        
         
         
         
