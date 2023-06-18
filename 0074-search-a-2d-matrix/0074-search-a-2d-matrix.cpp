af795c133cbda9c0489222e98c2dabad99024f81
Anunay <94173658+Anunayyy@users.noreply.github.com>
2023-06-18 22:49:36 +0530

    Time: 7 ms (27.82%), Space: 9.8 MB (25.73%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size();
-        int m = matrix[0].size();
+        int r = matrix.size();
+        int c = matrix[0].size();
         
-		// if matrix have 0 rows or 0 colums
-        if(n == 0 || m == 0)
-            return false;
+        int i=0;
+        int j=0;
+        stack<int>s;
         
-		// treating matrix as array just taking care of endices
-		// [0..n*m]
-        int start = 0, end = m*n - 1;
-        
-        while(start <= end)
-        {
-            int mid = start + (end - start) / 2;
-			// a[x] : matrix[x / m][x % m] formulae
-            int ind = matrix[mid/m][mid%m];
-            if (target == ind)
-                return true;
-			// left half
-            else if(target < ind)
-                end = mid - 1;
-            else
-			// right half
-                start = mid + 1;       
+        while(i<r){
+                s.push(matrix[i][j]);
+                if(s.top()==target) return true;
+                else if(j==c-1){
+                    i++;
+                    j=0;
+                    } 
+                else if(j!=c-1){
+                    j++;
+                }
+                
         }
         return false;
+        
     }
 };
