adee81d4d709476be2af525cd353e66cdf1d6727
Anunay <94173658+Anunayyy@users.noreply.github.com>
2023-07-04 01:09:52 +0530

    Time: 3 ms (78.38%), Space: 7.1 MB (19.49%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.cpp
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -1,10 +1,34 @@
 class Solution {
 public:
-        bool buddyStrings(string A, string B) {
-        if (A.length() != B.length()) return false;
-        if (A == B && set<char>(A.begin(), A.end()).size() < A.size()) return true;
-        vector<int> dif;
-        for (int i = 0; i < A.length(); ++i) if (A[i] != B[i]) dif.push_back(i);
-        return dif.size() == 2 && A[dif[0]] == B[dif[1]] && A[dif[1]] == B[dif[0]];
+    bool buddyStrings(string s, string goal) {
+        int cnt=0;
+        vector<int>v;
+        vector<int>fre(26,0);
+        
+        if(s.size()!=goal.size())return false;
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=goal[i]){
+                cnt++;
+                v.push_back(i);
+            }
+            fre[s[i]-'a']++;
+        }
+        
+        bool flag=false;
+        for(int i=0;i<26;i++){
+            if(fre[i]>=2){
+                flag=true;
+                break;
+            }
+            
+        }
+        if(cnt==0 and flag==true)return true;
+        if(cnt==0 and flag==false)return false;
+        if(cnt==1 or cnt>2) return false;
+        if(s[v[0]]==goal[v[1]] and s[v[1]]==goal[v[0]]) return true;
+        
+        return false;
+        
+        
     }
 };
