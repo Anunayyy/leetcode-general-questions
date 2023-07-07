ff0679858590d03566db0608447280122e777c94
Anunay <94173658+Anunayyy@users.noreply.github.com>
2023-07-08 04:57:20 +0530

    Time: 24 ms (83.22%), Space: 10.2 MB (32.66%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,46 +1,30 @@
 class Solution {
 public:
-    int maxConsecutiveAnswers(string answerKey, int k) {
-        int n = answerKey.length();
-        int left = k, right = n;
-        
-        while (left < right) {
-            int mid = (left + right + 1) / 2;
-            
-            if (isValid(answerKey, mid, k)) {
-                left = mid;
-            } else {
-                right = mid - 1;
+    int maxConsecutiveAnswers(string s, int k) {
+        int size=s.size(),left=0,res=0,cnt=0;
+      //  maximum substring of 'T' which contains k 'F'
+        for(int i=0;i<size;i++){
+            if(s[i]=='F')
+                cnt++;
+            while(cnt>k){
+                if(s[left]=='F')
+                    cnt--;
+                left++;
             }
+            res=max(i-left+1,res);
         }
-        
-        return left;
-    }
-    
-    bool isValid(string answerKey, int size, int k) {
-        int n = answerKey.length();
-        unordered_map<char, int> counter;
-        
-        for (int i = 0; i < size; i++) {
-            char c = answerKey[i];
-            counter[c]++;
-        }
-        
-        if (min(counter['T'], counter['F']) <= k) {
-            return true;
-        }
-        
-        for (int i = size; i < n; i++) {
-            char c1 = answerKey[i];
-            counter[c1]++;
-            char c2 = answerKey[i - size];
-            counter[c2]--;
-            
-            if (min(counter['T'], counter['F']) <= k) {
-                return true;
+    //maximum  substring of 'F' which contains k 'T'
+      cnt=0;left=0;
+      for(int i=0;i<size;i++){
+            if(s[i]=='T')
+                cnt++;
+            while(cnt>k){
+                if(s[left]=='T')
+                    cnt--;
+                left++;
             }
+            res=max(i-left+1,res);
         }
-        
-        return false;
+        return res;       
     }
 };
