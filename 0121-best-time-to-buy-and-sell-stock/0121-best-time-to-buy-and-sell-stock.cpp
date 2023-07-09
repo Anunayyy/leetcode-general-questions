c0d39cd7295f9dde04c03dde0572ca854c5b069f
Anunay <94173658+Anunayyy@users.noreply.github.com>
2023-07-10 03:09:46 +0530

    Time: 157 ms (27.85%), Space: 93.3 MB (71.57%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,3 +1,29 @@
+// class Solution {
+// public:
+    // int diff(vector<int>&nums,i,j){
+    //     int dif=nums[j]-nums[i];
+    //     if(dif>0)return dif;
+    //     else return 0;
+    // }
+    
+//     int maxProfit(vector<int>& nums) {
+//         int r=1;
+//         int l=0;
+//         int profit=0;
+//         int maxp=0;
+//         while(r<nums.size()){
+//             if(nums[l]<nums[r]){
+//                 profit=nums[r]-nums[l];
+//                 maxp=max(profit,maxp);
+//             }
+//             else{
+//                 l=r;
+//                 r=r+1;
+//             }
+//         }
+//         return profit;
+//     }
+// };
 class Solution {
 public:
     int maxProfit(vector<int>& price) {
