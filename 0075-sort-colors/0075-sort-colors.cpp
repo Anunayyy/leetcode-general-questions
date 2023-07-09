d3023290776349ab95f25869dc3e57a4c9fe77a9
Anunay <94173658+Anunayyy@users.noreply.github.com>
2023-07-09 21:20:00 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (81.96%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,44 +1,15 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n=nums.size();
-        int slow=0;
-        int fast=n-1;
-        int curr=0;
-        
-        while(curr<=fast){
-            if (nums[curr]==0){
-                swap(nums[slow],nums[curr]);
-                slow++;
-                curr++;
-            }
-            else if (nums[curr]==2){
-                swap(nums[fast],nums[curr]);
-                fast--;
-            }
-            else if (nums[curr]==1) curr++;
-        }
-        
+	int minInd, startInd, currInd, size = nums.size();
+	if (size <= 1) return;
+	for (startInd = 0; startInd < size - 1; startInd++) {
+		for (currInd = startInd + 1, minInd = startInd; currInd < size; currInd++)
+			if (nums[minInd] > nums[currInd])
+				minInd = currInd;
+		if (minInd != startInd)
+			swap(nums[startInd], nums[minInd]);
+	    }
     }
 };
 
-/*class Solution {
-public:
-    void sortColors(vector<int>& nums) {
-        int low = 0, mid = 0, high = nums.size()-1;
-        while(mid <= high){
-            if(nums[mid] == 0){
-                swap(nums[low], nums[mid]);
-                low++;
-                mid++;
-            }
-            else if(nums[mid] == 1){
-                mid++;
-            }
-            else{
-                swap(nums[mid], nums[high]);
-                high--;
-            }
-        }
-    }
-};*/
