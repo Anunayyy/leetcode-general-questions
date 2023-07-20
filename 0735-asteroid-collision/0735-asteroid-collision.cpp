74663f25bb548ea5fa8f7ea8e6aec20994f10964
Anunay <94173658+Anunayyy@users.noreply.github.com>
2023-07-20 16:17:24 +0530

    Time: 17 ms (48.92%), Space: 17.8 MB (24.17%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -4,20 +4,25 @@ public:
         stack<int>s;
         
         for(auto x:asteroids){
-            while(!s.empty() && x<0 && s.top()>0){
+            //now we only need to check for negative value and 3 cases for it
+            while(!s.empty() && x<0 && s.top()>0){ 
+                // case 1: if the next element is n=bigger that thetop of the stack
                 if(abs(x)>s.top()){
                     s.pop();
                     
                 }
+                // case 2: if the next element is equal to the top of the stack
                 else if(abs(x)==s.top()){
                     s.pop();
                     x=0;
                     break;
                 }
+                //case 3: if the next element is less than the top of the stack
                 else{
                     x=0;
                 }
             }
+            // for any other ase we will directly reach here so we kept in mind everything while solving this question
             
             if(x!=0){
                 s.push(x);
