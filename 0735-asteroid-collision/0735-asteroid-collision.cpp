1f9fd39dbae3234cb5ad6c08362ba50e6bf2cd9c
Anunay <94173658+Anunayyy@users.noreply.github.com>
2023-07-20 16:08:27 +0530

    Time: 21 ms (22.37%), Space: 17.8 MB (55.66%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,72 +1,37 @@
-/*class Solution {
-public:
-    vector<int> asteroidCollision(vector<int>& a) {
-        stack<int>s;
-        int n = a.size();
-        s.push(a[0]);
-        for(int i=1;i<n;i++){
-            if(s.top()<0){
-                s.push(a[i]);
-                }
-            else if(s.empty()){
-                s.push(a[i]);
-            }
-            else if(s.top()>0 and a[i]<0 and s.top()<abs(a[i])){
-                s.pop();
-            }
-            else if(s.top()>0 and a[i]<0 and s.top()==abs(a[i])){
-                s.pop();
-                i++;
-            }
-            else if (s.top()>0 and a[i]<0 and s.top()>abs(a[i]))i++;
-        }
-        vector<int>res;
-        int x = s.size();
-        for(int i=x-1;i>=0;i--){
-            res.push_back(s.top());
-            s.pop();
-            
-            
-        }
-        return res;
-                                                           
-        
-    }
-};*/
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int> s;
+        stack<int>s;
         
-        for (int asteroid : asteroids) {
-            bool destroyed = false;
-            
-            while (!s.empty() && asteroid < 0 && s.top() > 0) {
-                if (s.top() < abs(asteroid)) {
+        for(auto x:asteroids){
+            while(!s.empty() && x<0 && s.top()>0){
+                if(abs(x)>s.top()){
                     s.pop();
+                    
                 }
-                else if (s.top() == abs(asteroid)) {
+                else if(abs(x)==s.top()){
                     s.pop();
-                    destroyed = true;
+                    x=0;
                     break;
                 }
-                else {
-                    destroyed = true;
-                    break;
+                else{
+                    x=0;
                 }
             }
             
-            if (!destroyed) {
-                s.push(asteroid);
+            if(x!=0){
+                s.push(x);
             }
         }
-        
-        vector<int> res;
-        while (!s.empty()) {
-            res.insert(res.begin(), s.top());
+        vector<int> res(s.size());
+        for(int i = s.size()-1;i>=0;i--){
+            res[i]=s.top();
             s.pop();
         }
         
         return res;
-    }
+            
+        }
+        
+    
 };
